2c0c82210288201e49279f14d391164755cb57cb
vegesnasankeerthana <vegesnasankeerthana227@gmail.com>
2026-04-05 23:30:24 -0400

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
     ? import.meta.env.VITE_API_URL + '/api'
     : 'https://km-webpage-backend.onrender.com/api',
-  timeout: 60000,
+  timeout: 12000,
   headers: { 'Content-Type': 'application/json' },
 })
 
